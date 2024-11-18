e6454957e765a664710ff536fb08d50870114ead
Sam <samueltent98@gmail.com>
2024-11-18 23:49:07 +0200

    technical test solutions

---
+++ BalancedBrackets/BalancedBrackets/BalancedBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BalancedBrackets
+{
+    public class BalancedBrackets
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brackets input: \n");
+            string brackets = Console.ReadLine();
+
+            string result = IsBalanced(brackets) ? "Balanced" : "Not balanced";
+
+            Console.WriteLine(result);
+        }
+
+        public static bool IsBalanced(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(')
+                    stack.Push(')');
+                else if (c == '[')
+                    stack.Push(']');
+                else if (c == '{')
+                    stack.Push('}');
+                else if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ SingleNumber/SingleNumber/SingleNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace SingleNumber
+{
+    internal class SingleNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert integeres separated by comma(,): \n");
+
+            string input = Console.ReadLine();
+
+            int[] nums = input.Split(',').Select(s => Convert.ToInt32(s)).ToArray();
+
+            int singeNumber = IsSingleNumber(nums);
+
+            Console.WriteLine(singeNumber);
+        }
+
+        public static int IsSingleNumber(int[] nums)
+        {
+            int res = 0;
+
+            foreach (int num in nums)
+            {
+                res ^= num;
+            }
+
+            return res;
+        }
+    }
+}
